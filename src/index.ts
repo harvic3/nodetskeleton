8f491ef1ca773ab7e16f8de5b61c67cec568ff6c
Vickodev <harvic3@gmail.com>
2021-07-25 22:36:50 -0500

    import lib was moved out to controllers region

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import "express-async-errors";
 import AppWrapper from "./infrastructure/server/AppWrapper";
+import { HttpServer } from "./infrastructure/server/HttpServer";
 import handlerErrorMiddleware from "./infrastructure/middleware/error";
 import BaseController from "./adapters/controllers/base/BaseController";
 
 // Region controllers
 import healthController from "./adapters/controllers/health/HealthController";
 import authController from "./adapters/controllers/auth/AuthController";
-import { HttpServer } from "./infrastructure/server/HttpServer";
 // End controllers
 
 const controllers: BaseController[] = [healthController, authController];
