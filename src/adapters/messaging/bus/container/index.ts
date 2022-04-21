27ce0b6d95f937d4daaeb2ac5b33c4a3e9e6bb0e
Vickodev <harvic3@gmail.com>
2022-04-20 23:47:24 -0500

    refactor: change scoped to singleton instance in bus container

--- src/adapters/messaging/bus/container/index.ts
+++ src/adapters/messaging/bus/container/index.ts
@@ -5,16 +5,18 @@ import { EventPublisher } from "../publisher/EventPublisher";
 import { EventListener } from "../listener/EventListener";
 import { EventClientEnum } from "../EventClient.enum";
 
-const tskMessageListener = new EventListener(EventClientEnum.TSK_MESSAGE_BUS);
-const tskMessagePublisher = new EventPublisher(EventClientEnum.TSK_MESSAGE_BUS);
-const tskMessageSubscriber = new EventSubscriber(EventClientEnum.TSK_MESSAGE_BUS);
-
 const dictionary = new ContainerDictionary();
-dictionary.addScoped(EventClientEnum.TSK_BUS_LISTENER, (): EventListener => tskMessageListener);
-dictionary.addScoped(EventClientEnum.TSK_BUS_PUBLISHER, (): EventPublisher => tskMessagePublisher);
-dictionary.addScoped(
+dictionary.addSingleton(
+  EventClientEnum.TSK_BUS_LISTENER,
+  new EventListener(EventClientEnum.TSK_MESSAGE_BUS),
+);
+dictionary.addSingleton(
+  EventClientEnum.TSK_BUS_PUBLISHER,
+  new EventPublisher(EventClientEnum.TSK_MESSAGE_BUS),
+);
+dictionary.addSingleton(
   EventClientEnum.TSK_BUS_SUBSCRIBER,
-  (): EventSubscriber => tskMessageSubscriber,
+  new EventSubscriber(EventClientEnum.TSK_MESSAGE_BUS),
 );
 
 export { EventClientEnum, EventListener, EventPublisher, EventSubscriber };
