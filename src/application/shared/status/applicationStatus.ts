586cc96c421a4ec2b85e95558c1d0c7d41127c05
Vickodev <harvic3@gmail.com>
2022-08-13 12:18:53 -0500

    update: some application error codes was changed

--- src/application/shared/status/applicationStatus.ts
+++ src/application/shared/status/applicationStatus.ts
@@ -1,10 +1,10 @@
 export default {
-  SUCCESS: "20",
+  INTERNAL_ERROR: "FF",
+  SUCCESS: "00",
   CREATED: "21",
   NOT_CONTENT: "24",
   INVALID_INPUT: "40",
   UNAUTHORIZED: "41",
   NOT_FOUND: "44",
-  INTERNAL_ERROR: "50",
   WORKER_ERROR: "51",
 };
