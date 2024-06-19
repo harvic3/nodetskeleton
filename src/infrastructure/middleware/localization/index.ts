8cd1b7c6702d37677da0fdbef0ddb2aa52afe24c
Retamiro <retamiro@gmail.com>
2024-06-19 01:56:54 -0300

    PR adjustment

--- src/infrastructure/middleware/localization/index.ts
+++ src/infrastructure/middleware/localization/index.ts
@@ -7,33 +7,35 @@ import appWords from "../../../application/shared/locals/words";
 import { Middleware } from "../types";
 import config from "../../config";
 
-const getLanguage = (req: Request) => {
-  // "pt-BR,pt;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
-  try {
-    const locals = appMessages["values"];
-    const language = req.headers["accept-language"] || "";
-    const languages = language.split(",");
-    for (const key in languages) {
-      if (Object.prototype.hasOwnProperty.call(languages, key)) {
-        const element = languages[key];
-        const lang = element.replace(/;.+/, "");
-        if (locals[lang]) {
-          return lang as LocaleTypeEnum;
-        }
-      }
-    }
-  } catch {}
 
-  return config.Params.DefaultLanguage;
-};
 
 export class LocalizationMiddleware {
   handle: Middleware = (req: Request, _res: Response, next: NextFunction) => {
-    const locale = getLanguage(req);
+    const locale = LocalizationMiddleware.getLanguage(req);
     TypeParser.cast<IRequest>(req).locale = locale;
     appMessages.init(locale);
     appWords.init(locale);
     return next();
+  };
+
+  private static getLanguage(req: Request): LocaleTypeEnum {
+
+    try {
+      const locals = appMessages["values"];
+      const language = req.headers["accept-language"] || "";
+      const languages = language.split(",");
+      for (const key in languages) {
+        if (Object.hasOwn(languages, key)) {
+          const element = languages[key];
+          const lang = element.replace(/;.+/, "");
+          if (locals[lang]) {
+            return lang as LocaleTypeEnum;
+          }
+        }
+      }
+    } catch { }
+
+    return config.Params.DefaultLanguage;
   };
 }
 
