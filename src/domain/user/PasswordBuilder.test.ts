01ad180b00a4a459d4482a6fa5058b94860070ff
Retamiro <retamiro@gmail.com>
2024-06-24 21:50:35 -0300

    PR adjustment

--- src/domain/user/PasswordBuilder.test.ts
+++ src/domain/user/PasswordBuilder.test.ts
@@ -1,16 +1,17 @@
 import { describe, expect, it } from "@jest/globals";
-
 import { PasswordBuilder } from "./PasswordBuilder";
-
 import { StringUtil } from "../shared/utils/StringUtil";
 
 describe("Domain PasswordBuilder - describe", () => {
   it("must instantiate an invalid PasswordBuilder", () => {
+    // Arrange
     const email = StringUtil.EMPTY;
     const passwordBase64 = StringUtil.EMPTY;
 
+    // Act
     const passwordBuilder = new PasswordBuilder(email, passwordBase64);
 
+    // Assert
     expect(passwordBuilder.value).toEqual(`${email}-${passwordBase64}`);
     expect(passwordBuilder.passwordBase64).toEqual(passwordBase64);
     expect(passwordBuilder.isValid()).toBeFalsy();
