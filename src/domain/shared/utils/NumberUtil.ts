75b228432f4e418a32b74f070f8115c6548ef72d
Victor A Higuita <harvic3@proton.me>
2025-03-05 23:43:13 -0500

    [ADD]: added mock builder class to improve the mock builder objects

--- src/domain/shared/utils/NumberUtil.ts
+++ src/domain/shared/utils/NumberUtil.ts
@@ -2,4 +2,8 @@ export class NumberUtil {
   static isEvenNumber(value: number): boolean {
     return value % 2 === 0;
   }
+
+  static getRandom(params: { min: number; max: number }): number {
+    return Math.floor(Math.random() * (params.max - params.min + 1) + params.min);
+  }
 }
